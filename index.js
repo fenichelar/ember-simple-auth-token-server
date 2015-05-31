b6c34cd416de62f9d07c15d6bfb662d5c36786c5
José Padilla <jpadilla@webapplicate.com>
2015-05-31 10:40:20 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.post('/api/api-token-auth', function(req, res) {
+  var token = jwt.sign(req.body, 'secret', {expiresInSeconds: 10});
+  res.send({
+    token: token
+  });
+});
+
+app.post('/api/api-token-refresh', function(req, res) {
+  var decoded = jwt.verify(req.body.token, 'secret');
+
+  res.send({
+    token: jwt.sign(decoded, 'secret', {expiresInSeconds: 10})
+  });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Running on port', app.get('port'));
+});
