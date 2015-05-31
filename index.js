3fdb7c3ad582f0a8f07570ea5271b2259bc25027
José Padilla <jpadilla@webapplicate.com>
2015-05-31 11:21:51 -0400

    Tweak CORS settings

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
